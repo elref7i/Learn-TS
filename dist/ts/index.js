55932455aee04a3a45d3ff2124e3f40410441c39
Ahmed Refai <momo3956248@gmail.com>
2024-12-18 23:41:29 +0200

    Built-in Types

--- dist/ts/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-console.log('refai');
-const z = 'refai';
-//# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,15 @@
-console.log('refai');
-const z = 'refai';
+// * Built-in Types =>String ,number ,Boolean , null , undefined
+
+let userName: string = 'refai';
+
+userName = 'ahmed refai';
+
+let age: number = 21;
+age = 22;
+
+let checked: boolean = false;
+checked = true;
+
+let data: null = null;
+
+let x: undefined;
