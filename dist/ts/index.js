e323946eceabe65c6749151c2a63437d7ec2d13d
Ahmed Refai <momo3956248@gmail.com>
2024-12-19 00:15:11 +0200

    User Defined Types  [ Array , Unions ]

---
+++ dist/ts/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// * Built-in Types =>String ,number ,Boolean , null , undefined
+let userName = 'refai';
+userName = 'ahmed refai';
+let age = 21;
+age = 22;
+let checked = false;
+checked = true;
+let data = null;
+let x;
+//* User Defined Types ==> Array , Unions
+let empty = [];
+let frinds = ['ahmed', 'khaled', 'refai'];
+let numbers = [10, 20, 30, 40, 50];
+let employee = [22, 'Ahmed Khaled'];
+let userAge = 10;
+userAge = 'ten';
+//# sourceMappingURL=index.js.map
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -13,3 +13,16 @@ checked = true;
 let data: null = null;
 
 let x: undefined;
+
+//* User Defined Types ==> Array , Unions
+
+let empty: [] = [];
+
+let frinds: string[] = ['ahmed', 'khaled', 'refai'];
+
+let numbers: number[] = [10, 20, 30, 40, 50];
+
+let employee: (number | string)[] = [22, 'Ahmed Khaled'];
+
+let userAge: number | string = 10;
+userAge = 'ten';
